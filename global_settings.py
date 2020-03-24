340c58896e1273fb89462e60d2d43e8c4811e3df
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-03-24 22:50:49 +0000

    Delete global_settings.py

--- global_settings.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-links_df = pd.read_csv('tools/links.txt', sep='\t')
-link_df = pd.read_csv('tools/links.txt', sep='\t', encoding='latin1', dtype='str')
-DATA_FOLDER = '/Users/mmw/Documents/GitHub/ml_val/data_all'
