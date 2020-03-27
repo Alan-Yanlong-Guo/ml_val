ef1ff175c0f1e93dd256f3b4fee3b3fc69ba2bfd
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-03-27 20:44:44 +0000

    update

--- data/build_x.py
+++ data/build_x.py
@@ -2,6 +2,8 @@ from data.x_annual import build_comp, build_crsp_m, build_dlret, build_crsp, bui
 from data.x_quarter import build_compq6
 from data.x_month import build_temp6
 from global_settings import DATA_FOLDER, links_df
+from tools.utils import x_filter
+import pandas as pd
 import os
 import pickle
 import string
@@ -18,6 +20,11 @@ def run_build_ccm_jun(tics, group):
     ccm_jun['fqtr'] = 4
     ccm_jun = ccm_jun.set_index(['permno', 'fyear', 'fqtr'], inplace=False)
     ccm_jun = ccm_jun.sort_index(inplace=False)
+    ccm_jun_id = ccm_jun.iloc[:, :5]
+    ccm_jun = ccm_jun.iloc[:, 5:]
+    ccm_jun = x_filter(ccm_jun, 'annual')
+
+    ccm_jun = pd.concat([ccm_jun_id, ccm_jun], axis=1)
 
     with open(os.path.join(DATA_FOLDER, 'annual_x', '_'.join(['x', group]) + '.pkl'), 'wb') as handle:
         pickle.dump(ccm_jun, handle)
@@ -31,6 +38,11 @@ def run_build_compq6(tics, group, ccm_jun):
     compq6.rename(columns={'fyearq': 'fyear'}, inplace=True)
     compq6 = compq6.set_index(['permno', 'fyear', 'fqtr'], inplace=False)
     compq6 = compq6.sort_index(inplace=False)
+    compq6_id = compq6.iloc[:, :5]
+    compq6 = compq6.iloc[:, 5:]
+    compq6 = x_filter(compq6, 'quarter')
+
+    compq6 = pd.concat([compq6_id, compq6], axis=1)
 
     with open(os.path.join(DATA_FOLDER, 'quarter_x', '_'.join(['x', group]) + '.pkl'), 'wb') as handle:
         pickle.dump(compq6, handle)
@@ -42,6 +54,11 @@ def run_build_temp6(tics, group, temp2, compq6):
     temp6 = build_temp6(tics, temp2, compq6)
     temp6 = temp6.set_index(['permno', 'year', 'month'], inplace=False)
     temp6 = temp6.sort_index(inplace=False)
+    temp6_id = temp6.iloc[:, :5]
+    temp6 = temp6.iloc[:, 5:]
+    temp6 = x_filter(temp6, 'month')
+
+    temp6 = pd.concat([temp6_id, temp6], axis=1)
 
     with open(os.path.join(DATA_FOLDER, 'month_x', '_'.join(['x', group]) + '.pkl'), 'wb') as handle:
         pickle.dump(temp6, handle)
