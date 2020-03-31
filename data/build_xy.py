83e4aacdb19a46d34067296aaea6a0e607282293
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2020-03-31 16:57:17 +0100

    u

--- data/build_xy.py
+++ data/build_xy.py
@@ -75,6 +75,8 @@ def build_x_line(permno, x_annual, x_quarter, x_month, y_annual, y_quarter, indu
                             x_quarter.reset_index(drop=True), x_month.reset_index(drop=True),
                             y_annual.reset_index(drop=True), y_quarter.reset_index(drop=True),
                             industrial.reset_index(drop=True)], axis=1)
+        x_line.index = x_index.index
+
     else:
         x_line = pd.DataFrame(columns=list(x_index.columns) + list(x_annual.columns) + list(x_quarter.columns) +
                                       list(x_month.columns) + list(y_annual.columns) + list(industrial.columns))
@@ -100,6 +102,7 @@ def build_y_line(permno, y_annual, y_quarter, y_ay, y_qy, y_qq, date):
     if np.shape(y_index)[0] == 1 and np.shape(y_annual)[0] == 1 and np.shape(y_quarter)[0] == 1:
         y_line = pd.concat([y_index.reset_index(drop=True), y_annual.reset_index(drop=True),
                             y_quarter.reset_index(drop=True)], axis=1)
+        y_line.index = y_index.index
 
     else:
         y_line = pd.DataFrame(columns=list(y_index.columns) + list(y_annual.columns) + list(y_quarter.columns))
@@ -150,8 +153,6 @@ def run_build_xy(year, dy=1, dq=0, cf='c'):
         x_df_, y_df_ = build_xy(year, dy, dq, cf, group)
         x_df = pd.concat([x_df, x_df_], axis=0)
         y_df = pd.concat([y_df, y_df_], axis=0)
-        print(x_df)
-        print(y_df)
 
     folder = '_'.join(['xy', 'q', str(dy), str(dq)])
     if not os.path.exists(os.path.join(DATA_FOLDER, folder)):
