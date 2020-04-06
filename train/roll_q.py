602b52eb9d72d621ab4be187f26fe941dfea4e73
Mingwei Ma <54514033+mingweima@users.noreply.github.com>
2020-04-06 18:52:48 +0100

    Update roll_q.py

--- train/roll_q.py
+++ train/roll_q.py
@@ -27,9 +27,9 @@ def run_load_xy(years, dy=0, dq=1, save_dir='xy_data', aq='q'):
     return x_df_set, y_df_set
 
 
-def train_test_process(tr_start=1975, tr_duration=30, ts_duration=1, dy=0, dq=1, plot=False):
+def train_test_process(data_start=1975, tr_start=1975, tr_duration=20, ts_duration=1, dy=0, dq=1, plot=False):
 
-    tr_years = [tr_start+i for i in range(tr_duration)]
+    tr_years = [data_start+i for i in range(tr_duration+tr_start-data_start)]
     print(tr_years)
     ts_years = [tr_start+tr_duration+i for i in range(ts_duration)]
     print(ts_years)
@@ -54,77 +54,23 @@ def train_test_process(tr_start=1975, tr_duration=30, ts_duration=1, dy=0, dq=1,
     y_ts = y_ts.fillna(0).astype('float32')
 
     oosr2 = {}
-    oosr2single = {}
+    y_trues = {}
+    y_preds = {}
     max1 = {}
     max2 = {}
     max3= {}
     max4 = {}
     max5 = {}
 
-    def single_feature_oosr2(item, x_tr=x_tr, y_tr=y_tr, x_ts=x_ts, y_ts=y_ts,
-                             param_test = {'max_depth': [1]}):
-        try:
-            _ = x_tr[item].values.shape[1]
-            x_tr_single = x_tr[item].values[:, 0].reshape(-1, 1)
-            y_tr_single = y_tr[item].values
-            x_ts_single = x_ts[item].values[:, 0].reshape(-1, 1)
-            y_ts_single = y_ts[item].values
-        except IndexError:
-            x_tr_single = x_tr[item].values.reshape(-1, 1)
-            y_tr_single = y_tr[item].values
-            x_ts_single = x_ts[item].values.reshape(-1, 1)
-            y_ts_single = y_ts[item].values
-
-        mod = LGBMRegressor(objective='regression_l2', zero_as_missing=True)
-        clf = sklearn.model_selection.GridSearchCV(mod , param_grid = param_test, scoring='r2', cv=3)
-        clf.fit(x_tr_single, y_tr_single)
-        predictions = clf.predict(x_ts_single)
-
-        return sklearn.metrics.r2_score(y_ts_single, predictions)
-
-# #     level prediction
-
-#     for item in y_tr.columns:
-#         if '_5o5' in item or '_qoq' in item or '_aoa' in item:
-#             continue
-#         param_test = {'max_depth': [1, 6], 'num_leaves': [2, 4], 'n_estimators': [20, 100, 500]}
-# #         param_test = {'max_depth': [1], 'num_leaves': [2], 'n_estimators': [5]}
-#         mod = LGBMRegressor(objective='regression_l2', zero_as_missing=True)
-#         clf = sklearn.model_selection.GridSearchCV(mod , param_grid = param_test, scoring='r2', cv=3)
-#         clf.fit(x_tr.values, y_tr[item].values)
-#         predictions = clf.predict(x_ts.values)
-#         y_true = y_ts[item].values
-
-#         if plot is True:
-#             plt.bar(range(len(clf.best_estimator_.feature_importances_)), clf.best_estimator_.feature_importances_)
-#             plt.title(str(tr_start) + ' ' + item)
-#             plt.show()
-
-#         max_fea = []
-#         max_5 = clf.best_estimator_.feature_importances_.argsort()[-5:][::-1]
-#         for index in max_5:
-#             max_fea.append(x_tr.columns[index])
-#         oosr2[item] = sklearn.metrics.r2_score(y_true, predictions)
-#         try:
-#             sig_feature_oosr2 = single_feature_oosr2(item, param_test=param_test)
-#             enhancement = (oosr2[item] - sig_feature_oosr2)/sig_feature_oosr2
-#             print(tr_start, item, oosr2[item], enhancement, max_fea)
-#             print(clf.best_params_)
-#             oosr2single[item] = sig_feature_oosr2
-#         except KeyError:
-#             print(item, oosr2[item], max_fea)
-#             print(clf.best_params_)
-
-
     # year on year growth prediction
 
     for item in y_tr.columns:
-        if '_aoa' not in item:
+        if '_1o1' not in item:
             continue
-        param_test = {'max_depth': [1, 6], 'num_leaves': [2, 4], 'n_estimators': [20, 100, 500]}
-#         param_test = {'max_depth': [1], 'num_leaves': [2], 'n_estimators': [20, 100, 500]}
+        # param_test = {'max_depth': [1, 2, 4], 'num_leaves': [2, 6], 'n_estimators': [20, 100, 200]}
+        param_test = {'max_depth': [1], 'num_leaves': [2], 'n_estimators': [2]}
         mod = LGBMRegressor(objective='regression_l2', zero_as_missing=True)
-        clf = sklearn.model_selection.GridSearchCV(mod , param_grid = param_test, scoring='r2', cv=5)
+        clf = sklearn.model_selection.GridSearchCV(mod , param_grid = param_test, scoring='r2', cv=3)
         clf.fit(x_tr.values, y_tr[item].values)
         predictions = clf.predict(x_ts.values)
         y_true = y_ts[item].values
@@ -136,21 +82,17 @@ def train_test_process(tr_start=1975, tr_duration=30, ts_duration=1, dy=0, dq=1,
             max_fea.append(x_tr.columns[index])
         max1[item], max2[item], max3[item], max4[item], max5[item] = max_fea[0], max_fea[1], max_fea[2], max_fea[3], max_fea[4]
         oosr2[item] = sklearn.metrics.r2_score(y_true, predictions)
-        try:
-            sig_feature_oosr2 = single_feature_oosr2(item, param_test=param_test)
-            enhancement = (oosr2[item] - sig_feature_oosr2)/sig_feature_oosr2
-            oosr2single[item] = sig_feature_oosr2
-            print(tr_start, item, oosr2[item], enhancement, max_fea)
-            print(datetime.now(), clf.best_params_)
-        except KeyError:
-            print(item, oosr2[item], max_fea)
-            print(clf.best_params_)
+        print(tr_start, item, oosr2[item], max_fea)
+        print(datetime.now(), clf.best_params_)
+        y_trues[item] = y_trues
+        y_preds[item] = predictions
 
-    return oosr2, oosr2single, max1, max2, max3, max4, max5
+    return oosr2, max1, max2, max3, max4, max5, y_trues, y_preds
 
 
-oosr2df, oosr2singledf, max1df, max2df, max3df, max4df, max5df = None, None, None,None,None,None,None
-
+oosr2df, max1df, max2df, max3df, max4df, max5df = None, None, None, None, None, None
+y_true_array_dict = None
+y_pred_array_dict = None
 
 
 start_training_year = 1975
@@ -159,11 +101,21 @@ end_training_year = 2018
 
 for tr_start in range(start_training_year, end_training_year, 1):
 
-    oosr2, oosr2single, max1, max2, max3, max4, max5 = train_test_process(tr_start=tr_start, tr_duration=30, ts_duration=1, dy=0, dq=1)
+    oosr2, max1, max2, max3, max4, max5, y_trues, y_preds = train_test_process(data_start=start_training_year, tr_start=tr_start, tr_duration=3, ts_duration=1, dy=0, dq=1)
+    oosr2_cum = {}
+
+    if y_true_array_dict is None:
+        y_true_array_dict = y_trues
+        y_pred_array_dict = y_preds
+
+    else:
+        for key in y_trues.keys():
+            y_true_array_dict[key] = np.concatenate([y_true_array_dict[key], y_true[key]])
+            y_pred_array_dict[key] = np.concatenate([y_pred_array_dict[key], y_pred[key]])
+            oosr2_cum[key] = sklearn.metrics.r2_score(y_true_array_dict[key], y_pred_array_dict[key])
 
     if oosr2df is None:
         oosr2df = pd.DataFrame.from_dict(oosr2, orient='index', columns=[tr_start])
-        oosr2singledf = pd.DataFrame.from_dict(oosr2single, orient='index', columns=[tr_start])
         max1df = pd.DataFrame.from_dict(max1, orient='index', columns=[tr_start])
         max2df = pd.DataFrame.from_dict(max2, orient='index', columns=[tr_start])
         max3df = pd.DataFrame.from_dict(max3, orient='index', columns=[tr_start])
@@ -172,15 +124,15 @@ for tr_start in range(start_training_year, end_training_year, 1):
 
     else:
         oosr2df = pd.concat([oosr2df, pd.DataFrame.from_dict(oosr2, orient='index', columns=[tr_start])], axis=1)
-        oosr2singledf = pd.concat([oosr2singledf, pd.DataFrame.from_dict(oosr2single, orient='index', columns=[tr_start])], axis=1)
         max1df = pd.concat([max1df, pd.DataFrame.from_dict(max1, orient='index', columns=[tr_start])], axis=1)
         max2df = pd.concat([max2df, pd.DataFrame.from_dict(max2, orient='index', columns=[tr_start])], axis=1)
         max3df = pd.concat([max3df, pd.DataFrame.from_dict(max3, orient='index', columns=[tr_start])], axis=1)
         max4df = pd.concat([max4df, pd.DataFrame.from_dict(max4, orient='index', columns=[tr_start])], axis=1)
         max5df = pd.concat([max5df, pd.DataFrame.from_dict(max5, orient='index', columns=[tr_start])], axis=1)
 
+    oosr2df = pd.concat([oosr2df, pd.DataFrame.from_dict(oosr2_cum, orient='index', columns=['avg'])], axis=1)
+
     oosr2df.to_csv(os.path.join('/nfs/home/mingweim/ml_val/train/result', '_'.join(["oosr2", 'q', str(0), str(1), str(start_training_year), str(end_training_year)]) + '.csv'))
-    oosr2singledf.to_csv(os.path.join('~/ml_val/train/result', '_'.join(["oosr2single", 'q', str(0), str(1), str(start_training_year), str(end_training_year)]) + '.csv'))
     max1df.to_csv(os.path.join('/nfs/home/mingweim/ml_val/train/result', '_'.join(["max1", 'q', str(0), str(1), str(start_training_year), str(end_training_year)]) + '.csv'))
     max2df.to_csv(os.path.join('/nfs/home/mingweim/ml_val/train/result', '_'.join(["max2", 'q', str(0), str(1), str(start_training_year), str(end_training_year)]) + '.csv'))
     max3df.to_csv(os.path.join('/nfs/home/mingweim/ml_val/train/result', '_'.join(["max3", 'q', str(0), str(1), str(start_training_year), str(end_training_year)]) + '.csv'))
